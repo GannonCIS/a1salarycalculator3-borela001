55b6335162324aca74897d7c8d4b3b6d1783dceb
likhithborela <borela001@gannon.edu>
2020-02-13 23:56:32 -0500

    complete class structure for SalaryCalculator

---
+++ src/assignment1salarycalculator/SalaryCalculator.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1salarycalculator;
+
+
+/**
+ *
+ * @author gubotdev
+ */
+public class SalaryCalculator {
+
+    private int hoursInWeek = 40;
+    private int weeksInYear = 52;
+
+    public SalaryCalculator(){   
+
+    }
+    
+    public void getSalary(double hourlyWage){
+        
+    }
+
+    public void getHourlyWage(double salary){
+        
+    }
+
+    public void calcYearlyTaxes(double taxPercentage, double salary){
+        
+    }
+
+    public void monthlyAfterTax(double taxPercentage, double salary){
+        
+    }
+
+    public static void main(String[] args) {
+             
+    }
+    
+}
